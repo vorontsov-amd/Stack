b46130c9bfb13f60b0eab74045a92b201b44fd8c
vorontsov-amd <89128818+vorontsov-amd@users.noreply.github.com>
2021-11-05 23:04:21 +0300

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,22 +1,13 @@
-﻿#include "Header.h"
+#include "Header.h"
 
 
-
-int main()
-{
-	Stack stack = {};
-
-	StackCtor(&stack, 20);
-	StackPush(&stack, 10);
-	element value;
-	StackPop(&stack, &value);
-	StackDtor(&stack);
-	return 0;
-}
-
-void StackCtor(Stack* stack, size_t capaсity)
+void StackCtor(Stack* stack, long long capaсity)
 {
 	assert(stack);
+	if (capaсity < 0)
+	{
+		capaсity = 0;
+	}
 
 	stack->data = (element*)malloc(capaсity * sizeof(*stack->data) + 4 * sizeof(long long));
 	for (int i = 0; i < capaсity * sizeof(*stack->data) + 4 * sizeof(long long); i++)
@@ -36,11 +27,10 @@ void StackCtor(Stack* stack, size_t capaсity)
 	stack->upCanary2 = 0xFA11ED;
 	stack->downCanary1 = 0xFA11ED;
 	stack->downCanary2 = 0xFA11ED;
-	stack->hash.hashData = 0;
+
 	UpdateHash(stack);
 	CHECKSTACK;
 }
-
 
 
 void StackPush(Stack* stack, element value)
@@ -49,14 +39,14 @@ void StackPush(Stack* stack, element value)
 
 	if (stack->size < stack->capaсity)
 	{
-		*(element*)((char*)stack->data + 2 * sizeof(long long) + stack->size * sizeof(*stack->data)) = value;
+		StkData(stack->data, stack->size) = value;
 		stack->size++;
 		UpdateHash(stack);
 	}
 	else
 	{
 		StackResize(stack, stack->capaсity * 2);
-		*(element*)((char*)stack->data + 2 * sizeof(long long) + stack->size * sizeof(*stack->data)) = value;
+		StkData(stack->data, stack->size) = value;
 		stack->size++;
 		UpdateHash(stack);
 	}
@@ -65,7 +55,7 @@ void StackPush(Stack* stack, element value)
 }
 
 
-void StackResize(Stack* stack, size_t new_capaсity)
+void StackResize(Stack* stack, long long new_capaсity)
 {
 	CHECKSTACK;
 
@@ -93,7 +83,7 @@ void StackResize(Stack* stack, size_t new_capaсity)
 
 			for (int i = 0; i < stack->capaсity; i++)
 			{
-				*(element*)((char*)dataResize + 2 * sizeof(long long) + i * sizeof(*stack->data)) = StkData(i);
+				StkData(dataResize, i) = StkData(stack->data, i);
 			}
 
 			*(long long*)((char*)dataResize + 2 * sizeof(long long) + new_capaсity * sizeof(*stack->data)) = 0xFA11ED;
@@ -114,18 +104,22 @@ void StackPop(Stack* stack, element* value)
 	CHECKSTACK;
 	if (stack->size > 0)
 	{
-		*value = StkData(--stack->size);
+		*value = StkData(stack->data, --stack->size);
 		UpdateHash(stack);
 	}
-	else *value =  0;
+	else
+	{
+		*value = 0;
+		StackDump(stack, "Incorrect use of the stack. Trying to use StackPop function for stack size = 0");
+	}
 }
+
 
 void StackDtor(Stack* stack)
 {
 	CHECKSTACK;
 	free(stack->data);
 	stack->data = ERRPTR;
-	UpdateHash(stack);
 }
 
 
@@ -195,17 +189,17 @@ bool StackOk(Stack* stack)
 		stack->status = DATA_CANARY_DAMAGED;
 		status = false;
 	}
-	if (stack->hash.hashCapacity != SimpleHash(stack->capaсity, 1))
+	if (stack->hash.hashCapacity != ValueHash(stack->capaсity, 1))
 	{
 		stack->status = HASH_CAPACITY_MISMATCH;
 		status = false;
 	}
-	if (stack->hash.hashSize != SimpleHash(stack->size, 1))
+	if (stack->hash.hashSize != ValueHash(stack->size, 1))
 	{
 		stack->status = HASH_SIZE_MISMATCH;
 		status = false;
-	}
-	if (stack->hash.hashData != MurMurHash(stack->data, stack->capaсity, 1))
+	} 
+	if (stack->hash.hashData != StackHash(stack->data, stack->capaсity, 1))
 	{
 		stack->status = HASH_DATA_MISMATCH;
 		status = false;
@@ -230,26 +224,26 @@ void StackDump(const Stack* stack, const char* errmessage)
 	fprintf(errfile, "Stack [%p] (error number %d)\n\t{\n", stack, stack->status);
 	fprintf(errfile, "\t Canary 1 = %llX\n", stack->upCanary1);
 	fprintf(errfile, "\t Canary 2 = %llX\n", stack->upCanary2);
-	fprintf(errfile, "\t size = %zd\n", stack->size);
-	fprintf(errfile, "\t capacity = %zd\n", stack->capaсity);
+	fprintf(errfile, "\t size = %lld\n", stack->size);
+	fprintf(errfile, "\t capacity = %lld\n", stack->capaсity);
 	fprintf(errfile, "\t data [%p]\n\t\t{\n", stack->data);
 	fprintf(errfile, "\t\t C[0] = %llX\n", *(long long*)((char*)stack->data + 0));
 	fprintf(errfile, "\t\t C[1] = %llX\n", *(long long*)((char*)stack->data + sizeof(long long)));
 
 	if (stack->size > 0 and stack->capaсity > 0)
 	{
-		for (size_t i = 0; i < stack->size; i++)
+		for (long long i = 0; i < stack->size; i++)
 		{
-			fprintf(errfile, "\t\t *[%zd] = " EL "\n", i + 2, StkData(i));
+			fprintf(errfile, "\t\t *[%lld] = " EL "\n", i + 2, StkData(stack->data, i));
 		}
-		for (size_t i = stack->size; i <= stack->capaсity; i++)
+		for (long long i = stack->size; i <= stack->capaсity; i++)
 		{
-			fprintf(errfile, "\t\t  [%zd] = " EL "\n", i + 2, StkData(i));
+			fprintf(errfile, "\t\t  [%lld] = " EL "\n", i + 2, StkData(stack->data, i));
 		}
 	}
 	
-	fprintf(errfile, "\t\t C[%zd] = %llX\n", stack->capaсity + 2, *(long long*)((char*)stack->data + 2 * sizeof(long long) + stack->capaсity * sizeof(*stack->data)));
-	fprintf(errfile, "\t\t C[%zd] = %llX\n\t\t}\n", stack->capaсity + 3, *(long long*)((char*)stack->data + 2 * sizeof(long long) + stack->capaсity * sizeof(*stack->data) + sizeof(long long)));
+	fprintf(errfile, "\t\t C[%lld] = %llX\n", stack->capaсity + 2, *(long long*)((char*)stack->data + 2 * sizeof(long long) + stack->capaсity * sizeof(*stack->data)));
+	fprintf(errfile, "\t\t C[%lld] = %llX\n\t\t}\n", stack->capaсity + 3, *(long long*)((char*)stack->data + 2 * sizeof(long long) + stack->capaсity * sizeof(*stack->data) + sizeof(long long)));
 	fprintf(errfile, "\t Canary 3 = %llX\n", stack->downCanary1);
 	fprintf(errfile, "\t Canary 4 = %llX\n\t}\n", stack->downCanary2);
 }
