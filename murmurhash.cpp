e1f76e548982e6df7141a7586f3d1073dc5795e9
vorontsov-amd <89128818+vorontsov-amd@users.noreply.github.com>
2021-11-19 18:10:04 +0300

    Update murmurhash.cpp

--- murmurhash.cpp
+++ murmurhash.cpp
@@ -10,7 +10,7 @@ unsigned long long StackHash(element* data, long long capacity, int seed)
 
 	switch (sizeof(*data))
 	{
-	case 1: Hash8bit(data, capacity,  &hash); break;
+	case 1: Hash8bit(data, capacity, &hash); break;
 	case 2: Hash16bit(data, capacity, &hash); break;
 	case 4: Hash32bit(data, capacity, &hash); break;
 	case 8: Hash64bit(data, capacity, &hash); break;
@@ -29,7 +29,7 @@ unsigned long long RotateLeft(unsigned long long object, int n)
 unsigned long long CreateBlock8(char* data, size_t nBlock)
 {
 	return (data(0, FREQUENCY_CHAR, 56)) + (data(1, FREQUENCY_CHAR, 48)) + (data(2, FREQUENCY_CHAR, 40)) + (data(3, FREQUENCY_CHAR, 32)) +
-	   	   (data(4, FREQUENCY_CHAR, 24)) + (data(5, FREQUENCY_CHAR, 16)) + (data(6, FREQUENCY_CHAR, 8))  + (data(7, FREQUENCY_CHAR, 0));
+		(data(4, FREQUENCY_CHAR, 24)) + (data(5, FREQUENCY_CHAR, 16)) + (data(6, FREQUENCY_CHAR, 8)) + (data(7, FREQUENCY_CHAR, 0));
 }
 
 unsigned long long CreateBlock16(short* data, size_t nBlock)
@@ -55,7 +55,7 @@ unsigned long long CreateBlock64(long long* data, size_t nBlock)
 
 
 void Hash8bit(element* data, size_t size, unsigned long long* hash)
-{	
+{
 	size_t remaining_bytes = (size * sizeof(*data)) % 8;
 	size_t number64blocks = (size * sizeof(*data) - remaining_bytes) / 8;
 
@@ -74,21 +74,21 @@ void Hash8bit(element* data, size_t size, unsigned long long* hash)
 	switch (remaining_bytes)
 	{
 	case 7: k = (data(0, FREQUENCY_CHAR, 48)) + (data(1, FREQUENCY_CHAR, 40)) + (data(2, FREQUENCY_CHAR, 32)) +
-				(data(3, FREQUENCY_CHAR, 24)) + (data(4, FREQUENCY_CHAR, 16)) + (data(5, FREQUENCY_CHAR, 8)) +
-				(data(6, FREQUENCY_CHAR, 0));
+		(data(3, FREQUENCY_CHAR, 24)) + (data(4, FREQUENCY_CHAR, 16)) + (data(5, FREQUENCY_CHAR, 8)) +
+		(data(6, FREQUENCY_CHAR, 0));
 		break;
 	case 6: k = (data(0, FREQUENCY_CHAR, 40)) + (data(1, FREQUENCY_CHAR, 32)) + (data(2, FREQUENCY_CHAR, 24)) +
-				(data(3, FREQUENCY_CHAR, 16)) + (data(4, FREQUENCY_CHAR, 8))  + (data(5, FREQUENCY_CHAR, 0));
+		(data(3, FREQUENCY_CHAR, 16)) + (data(4, FREQUENCY_CHAR, 8)) + (data(5, FREQUENCY_CHAR, 0));
 		break;
 	case 5: k = (data(0, FREQUENCY_CHAR, 32)) + (data(1, FREQUENCY_CHAR, 24)) + (data(2, FREQUENCY_CHAR, 16)) +
-				(data(3, FREQUENCY_CHAR, 8))  + (data(4, FREQUENCY_CHAR, 0));
+		(data(3, FREQUENCY_CHAR, 8)) + (data(4, FREQUENCY_CHAR, 0));
 		break;
 	case 4: k = (data(0, FREQUENCY_CHAR, 24)) + (data(1, FREQUENCY_CHAR, 16)) + (data(2, FREQUENCY_CHAR, 8)) +
-				(data(3, FREQUENCY_CHAR, 0));
+		(data(3, FREQUENCY_CHAR, 0));
 		break;
-	case 3: k = (data(0, FREQUENCY_CHAR, 16)) + (data(1, FREQUENCY_CHAR, 8))  + (data(2, FREQUENCY_CHAR, 0));
+	case 3: k = (data(0, FREQUENCY_CHAR, 16)) + (data(1, FREQUENCY_CHAR, 8)) + (data(2, FREQUENCY_CHAR, 0));
 		break;
-	case 2: k = (data(0, FREQUENCY_CHAR, 8))  + (data(1, FREQUENCY_CHAR, 0));
+	case 2: k = (data(0, FREQUENCY_CHAR, 8)) + (data(1, FREQUENCY_CHAR, 0));
 		break;
 	case 1: k = (data(0, FREQUENCY_CHAR, 0));
 		break;
@@ -222,7 +222,7 @@ void Hash64bit(element* data, size_t size, unsigned long long* hash)
 unsigned long long ValueHash(long long value, int seed)
 {
 	unsigned long long hash = seed;
-	
+
 	unsigned long long k = value;
 	k *= c1;
 	k = RotateLeft(k, r1);
@@ -241,6 +241,8 @@ unsigned long long ValueHash(long long value, int seed)
 
 	return hash;
 }
+
+#undef data
 
 void UpdateHash(Stack* stack)
 {
